5ae45f5908bdb8bdf113319aef81d05bdf4b86b0
Efremov Denis <ruzs.efremov@mail.ru>
2021-10-12 13:22:25 +0300

    in model to template

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
+from .models import ProductCategory, Product
 
 def main(request):
-    context = {
-        'first_line': 'Информация, отображаемая на главной странице',
-        'second_word': 'второе слово',
-    }
-    return render(request, 'mainapp/index.html', context)
+    title = 'главная'
     
+    products = Product.objects.all()[:4]
+        
+    context = {'title': title, 'products': products}
+    return render(request, 'mainapp/index.html', context)    
 
 def products(request):
     return render(request, 'mainapp/products.html')
