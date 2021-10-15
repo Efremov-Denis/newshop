d4a58cde055baea7058aee6054008d35ca53a710
Efremov Denis <ruzs.efremov@mail.ru>
2021-10-15 19:45:12 +0300

    create registration mechanism

--- authapp/views.py
+++ authapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponseRedirect
-from authapp.forms import ShopUserLoginForm
+from authapp.forms import ShopUserLoginForm, ShopUserRegisterForm
 from django.contrib import auth
 from django.urls import reverse
 
@@ -22,3 +22,19 @@ def login(request):
 def logout(request):
     auth.logout(request)
     return HttpResponseRedirect(reverse('main'))     
+
+def register(request):
+    title = 'регистрация'
+    
+    if request.method == 'POST':
+        register_form = ShopUserRegisterForm(request.POST, request.FILES)
+    
+        if register_form.is_valid():
+            register_form.save()
+            return HttpResponseRedirect(reverse('auth:login'))
+    else:
+        register_form = ShopUserRegisterForm()
+    
+    context = {'title': title, 'register_form': register_form}
+    
+    return render(request, 'authapp/register.html', context)
