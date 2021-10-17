5abed85ea3f8b330016978498b3b87991106d43b
Efremov Denis <ruzs.efremov@mail.ru>
2021-10-17 21:12:53 +0300

    display of articles by category

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import ProductCategory, Product
 
 def main(request):
@@ -9,9 +9,39 @@ def main(request):
     context = {'title': title, 'products': products}
     return render(request, 'mainapp/index.html', context)    
 
-def products(request):
-    return render(request, 'mainapp/products.html')
+def products(request, pk=None):
+    print(pk)
     
+    title = 'продукты'
+    links_menu = ProductCategory.objects.all()
+            
+    if pk is not None:
+        if pk == 0:
+            products = Product.objects.all().order_by('price')
+            category = {'name': 'все'}
+        else:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            products = Product.objects.filter(category__pk=pk).order_by('price')
+
+        context = {
+            'title': title,
+            'links_menu': links_menu,
+            'category': category,
+            'products': products,
+        }
+
+        return render(request, 'mainapp/products_list.html', context)
+
+    
+    same_products = Product.objects.all()[3:5]
+    
+    context = {
+        'title': title, 
+        'links_menu': links_menu, 
+        'same_products': same_products
+    }
+    
+    return render(request, 'mainapp/products.html', context)
 
 def contacts(request):
     return render(request, 'mainapp/contacts.html')
