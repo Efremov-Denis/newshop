fe5d4cd5454b813782a2291653e8cf70e12aa30e
Efremov Denis <ruzs.efremov@mail.ru>
2021-10-20 17:30:11 +0300

    CRUD for shop objects users

--- adminapp/views.py
+++ adminapp/views.py
@@ -1,31 +1,72 @@
 from authapp.models import ShopUser
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, HttpResponseRedirect
 from mainapp.models import Product, ProductCategory
-
+from django.urls import reverse
+from authapp.forms import ShopUserRegisterForm
+from adminapp.forms import ShopUserAdminEditForm
 
 def users(request):
     title = 'админка/пользователи'
 
     users_list = ShopUser.objects.all().order_by('-is_active', '-is_superuser', '-is_staff', 'username')
 
-    content = {
+    context = {
         'title': title,
         'objects': users_list
     }
 
-    return render(request, 'adminapp/users.html', content)
+    return render(request, 'adminapp/users.html', context)
 
 
 def user_create(request):
-    pass
+    title = 'пользователи/создание'
 
+    if request.method == 'POST':
+        user_form = ShopUserRegisterForm(request.POST, request.FILES)   
+        if user_form.is_valid():
+            user_form.save()
+            return HttpResponseRedirect(reverse('admin:users'))
+    else:
+        user_form = ShopUserRegisterForm()
+    
+    context = {'title': title, 'update_form': user_form}
+    
+    return render(request, 'adminapp/user_update.html', context)
 
 def user_update(request, pk):
-    pass
+    title = 'пользователи/редактирование'
 
+    edit_user = get_object_or_404(ShopUser, pk=pk)
+    if request.method == 'POST':
+        edit_form = ShopUserAdminEditForm(request.POST, request.FILES,\
+                                                        instance=edit_user)
+        if edit_form.is_valid():
+            edit_form.save()
+            return HttpResponseRedirect(reverse('admin:user_update',\
+                                                args=[edit_user.pk]))
+    else:
+        edit_form = ShopUserAdminEditForm(instance=edit_user)
+    
+    context = {'title': title, 'update_form': edit_form}
+    
+    return render(request, 'adminapp/user_update.html', context)
 
 def user_delete(request, pk):
-    pass
+    title = 'пользователи/удаление'
+    
+    user = get_object_or_404(ShopUser, pk=pk)
+    
+    if request.method == 'POST':
+        #user.delete()
+        #вместо удаления лучше сделаем неактивным
+        user.is_active = False
+        user.save()
+        return HttpResponseRedirect(reverse('admin:users'))
+
+    context = {'title': title, 'user_to_delete': user}
+    
+    return render(request, 'adminapp/user_delete.html', context)
+
 
 
 def categories(request):
@@ -33,12 +74,12 @@ def categories(request):
 
     categories_list = ProductCategory.objects.all()
 
-    content = {
+    context = {
         'title': title,
         'objects': categories_list
     }
 
-    return render(request, 'adminapp/categories.html', content)
+    return render(request, 'adminapp/categories.html', context)
 
 
 def category_create(request):
@@ -59,13 +100,13 @@ def products(request, pk):
     category = get_object_or_404(ProductCategory, pk=pk)
     products_list = Product.objects.filter(category__pk=pk).order_by('name')
 
-    content = {
+    context = {
         'title': title,
         'category': category,
         'objects': products_list,
     }
 
-    return render(request, 'adminapp/products.html', content)
+    return render(request, 'adminapp/products.html', context)
 
 
 def product_create(request, pk):
